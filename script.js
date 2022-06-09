48e8e3a478079b429c4bdbe9c761dde10ab625eb
stantoha <78742661+stantoha@users.noreply.github.com>
2022-06-10 02:50:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let planets=document.querySelectorAll('.planet img'),
-planetsDiameter=[280,1,2.4,2.6,1.4,28,23.6,10.2,9.8];
+planetsDiameter=[280,1 ,2.4,2.6,1.4,28,47.2,20.4,9.8];
 
 
 planets.forEach((item,i)=>{
     item.style.width=planetsDiameter[i]+'px';
     item.style.height=planetsDiameter[i]+'px';
-    item.parentElement.style.width=planetsDiameter[i]+5+'px';
-    item.parentElement.style.height=planetsDiameter[i]+5+'px';
+    item.parentElement.style.width=planetsDiameter[i]-15+'px';
+    item.parentElement.style.height=planetsDiameter[i]-15+'px';
+    item.parentElement.classList.toggle('planet__shadow');
+    if(i%2!==true){
+        
+        item.classList.toggle('replace__left');
+    }
+    else{
+        item.classList.toggle('replace__right');
+    }
+    
 });
 console.log(planets);
